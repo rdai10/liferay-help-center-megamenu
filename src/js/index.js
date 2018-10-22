054f16309b443a9aaf22aee1f02d14181a8cac5e
Rebecca Dai <rhdai10@gmail.com>
2018-10-22 11:10:22 -0700

    export renderer

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-export {default as Card} from './components/Card';
-export {default as CardMenu} from './components/CardMenu';
-export {default as MegaMenu} from './components/MegaMenu';
+import render from './helper/preact-renderer';
+
+import Card from './components/Card';
+import CardMenu from './components/CardMenu';
+import MegaMenu from './components/MegaMenu';
+
+export {render, Card, CardMenu, MegaMenu};
