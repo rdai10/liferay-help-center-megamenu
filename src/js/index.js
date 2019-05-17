da0e08a81a5e5459b05b6cc0e4ab19e430f3bd1c
Rebecca Dai <rhdai10@gmail.com>
2019-05-17 16:12:12 -0700

    remove event helper

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 import render from './helper/preact-renderer';
-import {clickOutside} from './helper/event-helper';
 
 import Card from './components/Card';
 import CardMenu from './components/CardMenu';
 import MegaMenu from './components/MegaMenu';
 
-export {render, clickOutside, Card, CardMenu, MegaMenu};
+export {render, Card, CardMenu, MegaMenu};
